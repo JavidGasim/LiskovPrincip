b4266d2c9cf7892e9506b85b6611f2832f9876b1
JavidGasim <143398895+JavidGasim@users.noreply.github.com>
2023-12-03 16:55:51 +0400

    Add files via upload

---
+++ LiskovBacimiz/LiskovBacimiz/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace LiskovBacimiz;
+
+public class Shape
+{
+    public virtual void Draw()
+    {
+        Console.WriteLine("Drawing");
+    }
+}
+
+public class Rectangle : Shape
+{
+    public override void Draw()
+    {
+        Console.WriteLine("Rectancle is Drawing");
+    }
+}
+
+public class Circle : Shape
+{
+    public override void Draw()
+    {
+        Console.WriteLine("Circle is Drawing");
+    }
+}
+
+class Program
+{
+    static void DrawShape(Shape shape)
+    {
+        shape.Draw();
+    }
+
+    static void Main()
+    {
+        Shape shape1 = new Rectangle();
+        Shape shape2 = new Circle();
+
+        DrawShape(shape1); 
+        DrawShape(shape2);
+    }
+}
